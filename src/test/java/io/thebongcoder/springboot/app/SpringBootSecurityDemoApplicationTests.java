06c8ddfa4a64cfc299daacd01a87b2d83b058314
musibs <musib.somnath@gmail.com>
2020-01-01 22:20:28 +1100

    Initial commit

---
+++ src/test/java/io/thebongcoder/springboot/app/SpringBootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thebongcoder.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
