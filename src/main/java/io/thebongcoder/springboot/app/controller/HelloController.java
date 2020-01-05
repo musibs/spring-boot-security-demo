b847ae15f569710b83beaddaf1e0603c9f479bd0
musibs <musib.somnath@gmail.com>
2020-01-05 14:15:47 +1100

    Updated dependencies

--- src/main/java/io/thebongcoder/springboot/app/controller/HelloController.java
+++ src/main/java/io/thebongcoder/springboot/app/controller/HelloController.java
@@ -1,14 +1,27 @@
 package io.thebongcoder.springboot.app.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHello(){
+    public String sayHello(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userName;
+        int i;
+        if(principal instanceof UserDetails){
+            userName = ((UserDetails) principal).getUsername();
+        }
+        else{
+            userName = principal.toString();
+        }
+        System.out.println("User name is "+userName);
+        model.addAttribute("userName", userName);
         return "hello";
     }
 
